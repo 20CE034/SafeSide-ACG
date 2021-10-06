1dd66af9afdc12b6571f9144f52a6efbbc56516f
PlushWorks <77117781+Plushb9rry@users.noreply.github.com>
2021-10-06 10:57:03 +0530

    Delete SafeSideACG directory

--- SafeSideACG/JAVA2WEB(messed i=up)/Thirdjava.js
+++
@@ -1,7 +0,0 @@
-function printDiv(leftbox) {
-    var printContents = document.getElementById(leftbox).innerHTML;
-    w=window.open();
-    w.document.write(printContents);
-    w.print();
-    w.close();
-}
